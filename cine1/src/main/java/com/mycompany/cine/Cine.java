f92956c0c0174435722f865612a5fbb7da6b8324
cristhianveloz205 <cristhian.veloz.205@gmail.com>
2026-03-04 14:37:26 -0500

    Update Cine.java

--- cine1/src/main/java/com/mycompany/cine/Cine.java
+++ cine1/src/main/java/com/mycompany/cine/Cine.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.mycompany.cine") // Busca los Controladores y Servicios
-@EntityScan(basePackages = "com.mycompany.cine.model") // Busca las tablas de la BD
-@EnableJpaRepositories(basePackages = "com.mycompany.cine.repository") // Busca las consultas SQL
+@ComponentScan(basePackages = "com.mycompany.cine") 
+@EntityScan(basePackages = "com.mycompany.cine.model") 
+@EnableJpaRepositories(basePackages = "com.mycompany.cine.repository") 
 public class Cine {
     public static void main(String[] args) {
         SpringApplication.run(Cine.class, args);
