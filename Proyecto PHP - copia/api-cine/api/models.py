cef438289753fe7da61a0fad79c5bf0306549c48
cristhianveloz205 <cristhian.veloz.205@gmail.com>
2026-03-09 09:25:47 -0500

    Proyecto Php - Carpeta completa

---
+++ Proyecto PHP - copia/api-cine/api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class SalaCine(models.Model):
+    id_sala = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    estado = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'sala_cine'
+
+class Pelicula(models.Model):
+    id_pelicula = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    duracion = models.IntegerField(help_text="Duración en minutos")
+    estado_logico = models.BooleanField(default=True) # Para las eliminaciones lógicas
+
+    class Meta:
+        db_table = 'pelicula'
+
+class PeliculaSalaCine(models.Model):
+    id_pelicula_sala = models.AutoField(primary_key=True)
+    id_sala_cine = models.ForeignKey(SalaCine, on_delete=models.CASCADE, db_column='id_sala_cine')
+    id_pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE, db_column='id_pelicula')
+    fecha_publicacion = models.DateField()
+    fecha_fin = models.DateField()
+
+    class Meta:
+        db_table = 'pelicula_salacine'
