f7dead2e0b3e9f24b22738cae3b909aa8e5e6425
cristhianveloz205 <cristhian.veloz.205@gmail.com>
2026-03-10 10:45:49 -0500

    Delete Proyecto PHP - copia/api-cine directory

--- Proyecto PHP - copia/api-cine/config/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .controllers import PeliculaController, PeliculaDetalleController
-
-urlpatterns = [
-    path('peliculas/', PeliculaController.as_view()),
-    path('peliculas/<int:pk>/', PeliculaDetalleController.as_view()),
-]
