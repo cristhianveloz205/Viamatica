e59283b9ace786dc25dc5be62e91d02ad4fc9b5f
cristhianveloz205 <cristhian.veloz.205@gmail.com>
2026-03-13 16:37:37 -0500

    Update SalaCineService.java

--- cine1/src/main/java/com/mycompany/cine/service/SalaCineService.java
+++ cine1/src/main/java/com/mycompany/cine/service/SalaCineService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SalaCineService {
     @Autowired private SalaCineRepository salaCineRepo;
-
+// Regla de negocio: Determina el estado de la sala según el conteo de películas
     public String validarSala(String nombreSala) {
         Integer resultado = salaCineRepo.contarPeliculasPorSala(nombreSala);
         int cantidad = (resultado != null) ? resultado : 0; 
