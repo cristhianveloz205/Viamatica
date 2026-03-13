888363f6077256e05ebff9bc1147c52c0550598c
cristhianveloz205 <cristhian.veloz.205@gmail.com>
2026-03-13 16:36:32 -0500

    Update PeliculaRepository.java

--- cine1/src/main/java/com/mycompany/cine/repository/PeliculaRepository.java
+++ cine1/src/main/java/com/mycompany/cine/repository/PeliculaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PeliculaRepository extends JpaRepository<Pelicula, Integer> {
+    // Queries derivados para filtrar únicamente los registros que no han sido eliminados lógicamente
     List<Pelicula> findByEstadoLogicoTrue();
     List<Pelicula> findByNombreContainingIgnoreCaseAndEstadoLogicoTrue(String nombre);
 }
