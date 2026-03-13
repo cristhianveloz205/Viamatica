16755ee92d3c2f418d0617e95dbd350ce418c3c4
cristhianveloz205 <cristhian.veloz.205@gmail.com>
2026-03-13 16:35:25 -0500

    Update SalaCineRepository.java

--- cine1/src/main/java/com/mycompany/cine/repository/SalaCineRepository.java
+++ cine1/src/main/java/com/mycompany/cine/repository/SalaCineRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface SalaCineRepository extends JpaRepository<SalaCine, Integer> {
+    // Ejecuta la función almacenada nativa de MySQL para validar la disponibilidad
     @Query(value = "SELECT contar_peliculas_por_sala(:nombreSala)", nativeQuery = true)
     Integer contarPeliculasPorSala(@Param("nombreSala") String nombreSala);
 }
